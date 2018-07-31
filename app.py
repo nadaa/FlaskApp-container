25289a084b64674522799dab52b47a501710a611
nadaa <nada.saif7@gmail.com>
2018-07-31 05:11:33 -0700

    update

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app=Flask(__name__)
 def Home():
 	return "<h1>Welcome to the world of containers</h1>"
 
-
 if(__name__=='__main__'):
 	# as we run flask from the container, which is another domain, to tell falsk to accept any comming requests from different ip (host='0.0.0.0') is used.
 	app.run(host='0.0.0.0')
